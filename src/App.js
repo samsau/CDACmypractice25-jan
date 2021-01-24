6510eae7531aa42418e193395fff670881c09f1e
samsau <1355saurabh@gmail.com>
2021-01-25 00:52:46 +0530

    router DONE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Link to="/">Home</Link> | <Link to="/page1">Page1</Link> |
+				<Link to="/page2">Page2</Link> | <Link to="/page3">Page3</Link>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/page1" component={Page1}>
+					{/* <Page1 /> */}
+				</Route>
+				<Route exact path="/page2" component={Page2}>
+					{/* <Page2 /> */}
+				</Route>
+				<Route exact path="/page3" component={Page3}>
+					{/* <Page3 /> */}
+				</Route>
+			</Router>
+		</div>
+	);
 }
 
+function Home() {
+	return <div>Home</div>;
+}
+function Page1() {
+	return <div>Page1</div>;
+}
+function Page2() {
+	return <div>Page2</div>;
+}
+function Page3() {
+	return <div>Page3</div>;
+}
 export default App;
